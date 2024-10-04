965cd04001a186fa8aa908fb6eefe1557c2cedaf
Wasim Hyder <wasimhyder09@gmail.com>
2024-10-04 10:13:01 +0500

    Created job controllers and seed data with help of factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,22 +2,23 @@
 
 namespace Database\Seeders;
 
+use App\Models\Job;
 use App\Models\User;
+
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
-    {
-        // User::factory(10)->create();
+class DatabaseSeeder extends Seeder {
+  /**
+   * Seed the application's database.
+   */
+  public function run(): void {
+    // User::factory(10)->create();
+    Job::factory(100)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
-    }
+//    User::factory()->create([
+//      'name' => 'Test User',
+//      'email' => 'test@example.com',
+//    ]);
+  }
 }
