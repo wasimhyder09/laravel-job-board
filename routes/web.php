53d4525d23d5abe268fc9d8d3bd92227148b88eb
Wasim Hyder <wasimhyder09@gmail.com>
2024-10-05 08:30:39 +0500

    Created job tag component and listed more job info

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,6 @@
 use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('', fn() => to_route('jobs.index'));
+
 Route::resource('jobs', JobController::class)->only(['index']);
