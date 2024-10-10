700a9867039e541119506bcb3d1423d6e46754f9
Wasim Hyder <wasimhyder09@gmail.com>
2024-10-10 19:54:57 +0500

    Created custom middleware and protected routes with it

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\EmployerController;
 use App\Http\Controllers\JobApplicationController;
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\MyJobApplicationController;
+use App\Http\Controllers\MyJobController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('', fn() => to_route('jobs.index'));
@@ -21,4 +22,5 @@ Route::middleware('auth')->group(function () {
   Route::resource('job.application', JobApplicationController::class)->only('create', 'store');
   Route::resource('my-job-applications', MyJobApplicationController::class)->only('index', 'destroy');
   Route::resource('employer', EmployerController::class)->only('create', 'store');
+  Route::middleware('employer')->resource('my-jobs', MyJobController::class);
 });
