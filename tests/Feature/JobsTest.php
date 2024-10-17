254e059c777d392c184abb55606d5a1828486f93
Wasim Hyder <wasimhyder09@gmail.com>
2024-10-17 20:10:07 +0500

    Wrote test to see if an authenticated user can assess a page

--- tests/Feature/JobsTest.php
+++ tests/Feature/JobsTest.php
@@ -3,17 +3,18 @@
 namespace Tests\Feature;
 
 use App\Models\Job;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class JobsTest extends TestCase {
-  public function test_jobs_table_is_empty(): void {
-    $response = $this->get('/jobs');
-
-    $response->assertStatus(200);
-    $response->assertSee('No jobs found!');
-  }
+//  public function test_jobs_table_is_empty(): void {
+//    $response = $this->get('/jobs');
+//
+//    $response->assertStatus(200);
+//    $response->assertSee('No jobs found!');
+//  }
 
   public function test_jobs_table_is_not_empty(): void {
     $response = $this->get('/jobs');
@@ -31,6 +32,12 @@ class JobsTest extends TestCase {
     $response->assertViewHas('jobs', function ($collection) use ($job) {
       return $collection->contains($job);
     });
+  }
+
+  public function test_user_has_access_to_page() : void {
+    $user = User::findOrFail(1);
+    $response = $this->actingAs($user)->get('/my-jobs');
+    $response->assertStatus(200);
   }
 
 }
