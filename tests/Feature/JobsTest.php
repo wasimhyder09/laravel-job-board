807afc2a64a4ee228aeb517df8fa048a865afe2e
Wasim Hyder <wasimhyder09@gmail.com>
2024-10-17 14:47:53 +0500

    Tested viewhas query

--- tests/Feature/JobsTest.php
+++ tests/Feature/JobsTest.php
@@ -2,18 +2,35 @@
 
 namespace Tests\Feature;
 
+use App\Models\Job;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class JobsTest extends TestCase {
-  /**
-   * Check if jobs table is empty or not
-   */
   public function test_jobs_table_is_empty(): void {
     $response = $this->get('/jobs');
 
     $response->assertStatus(200);
     $response->assertSee('No jobs found!');
   }
+
+  public function test_jobs_table_is_not_empty(): void {
+    $response = $this->get('/jobs');
+
+    $response->assertStatus(200);
+    $response->assertDontSee('No jobs found!');
+  }
+
+  public function test_jobs_variable_has_values(): void {
+    $job = Job::findOrFail(1);
+
+    $response = $this->get('/jobs');
+
+    $response->assertStatus(200);
+    $response->assertViewHas('jobs', function ($collection) use ($job) {
+      return $collection->contains($job);
+    });
+  }
+
 }
